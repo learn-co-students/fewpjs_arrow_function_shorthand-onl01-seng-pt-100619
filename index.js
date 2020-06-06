dc9a6fa05aa38df37a5e165c9f4416459e8ab617
Amber Taranto <ambzzz@gmail.com>
2020-06-06 16:32:27 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide() {
+    return 2000 / 100
+}
+
+// let divide = (parameter1, parameter2) => {
+//     parameter2 / parameter1
+// }
+
+let square = num => num * num
+
+let add = (param1, param2) => param1 + param2
